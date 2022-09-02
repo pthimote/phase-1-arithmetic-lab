d983393526fc93ef50b8ca3abcac92e6e9e072d5
Paul Thimote <pthimote@gmail.com>
2022-09-01 20:53:59 -0400

    commit tree 9307fde58471e5c18442d3940755bb4fa2fece7d

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+Number.isInteger(1);
+const random = 1;
+
+const num3 = 8;
+const num4 = 20;
+const mod = num4 % num3;
+
+const max = Math.max(12, 20, 10);
